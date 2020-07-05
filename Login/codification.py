631c433d5377252c379d2d2e8ca93e275e0a0898
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-05 11:31:00 -0300

    password_codification

---
+++ Login/codification.py
@@ -0,0 +1,18 @@
+def code(string):
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    numbers = ["1","2","3","4","5","6","7","8","9","0"]
+    letters_codification = ["1","2","3","4","5","6","7","8","9","0", '1!', '2@', '3#', '4$', '6¨', '7&', '8*', '9(', '0)', '{', '}', '[', ']', '?', '/']
+    numbers_codification = ["!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+    
+    new_string = ''
+    for character in string:
+        try:
+            int(character)
+            character = numbers_codification[numbers.index(character)]
+        except:
+            character = character.lower()
+            character = letters_codification[letters.index(character)]
+        finally:
+            new_string += character
+            
+    return new_string
--- Login/main.py
+++ Login/main.py
@@ -1,14 +1,15 @@
 import getpass
+from codification import code
 
-def program():
+def Login():
     print('--------------------Login--------------------')
-    options = int(input('Digite 0 para cadastrar ou 1 para continuar para o Login: '))
+    options = int(input('Digite 0 para cadastrar ou 1 para logar: '))
 
     if options == 0:
         new_user = input('Crie um usuário: ')
         new_password = getpass.getpass('Crie uma senha: ')
         new_password_confirmation = getpass.getpass('Confirme sua senha: ')
-        user_data = ['user:({}) password:({}) \n'.format(new_user, new_password)]
+        user_data = ['user:({}) password:({}) \n'.format(new_user, code(new_password))]
 
         with open('database.txt', 'a') as file:
             for data in user_data:
@@ -16,17 +17,17 @@ def program():
         
         if new_password != new_password_confirmation:
             print('A senha criada é diferente da senha digitada na confirmação. Tente novamente.')
-            program()
+            Login()
         else:
             print("Usuário cadastrado com sucesso!")
-    
-    user = input("Usuário: ")
-    password = getpass.getpass("Senha: ")
-    with open('database.txt', 'r') as file:
-        data = file.read()
-        if data.find('user:({}) password:({})'.format(user, password)) != -1:
-            print('Bem Vindo {}!'.format(user))
-        else:
-            print('O Usuário e/ou senha estão incorretos.')
+    else:
+        user = input("Usuário: ")
+        password = getpass.getpass("Senha: ")
+        with open('database.txt', 'r') as file:
+            data = file.read()
+            if data.find('user:({}) password:({})'.format(user, code(password))) != -1:
+                print('Bem Vindo {}!'.format(user))
+            else:
+                print('O Usuário e/ou senha estão incorretos.')
         
-program()
+Login()
